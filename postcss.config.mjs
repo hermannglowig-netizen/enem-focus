6b454be1fac6c12342c89b3962a3ff9e6acf0d34
hermann <hermannglowig@gmail.com>
2026-05-01 23:23:39 -0300

    Corrigindo conflito no postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-<<<<<<< HEAD
-    '@tailwindcss/postcss': {}, // O erro acontece porque aqui estava apenas 'tailwindcss'
-    autoprefixer: {},
-=======
-    '@tailwindcss/postcss': {}, // É essa linha que resolve o erro do seu print!
-    'autoprefixer': {},
->>>>>>> 0c2212d (Primeiro deploy do Saas)
+    tailwindcss: {},
   },
 };
 
